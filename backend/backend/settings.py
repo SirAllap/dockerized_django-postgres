55227033326080a20e617c2c30fec657e3eda215
David PR <serallapdivad@gmail.com>
2024-09-10 18:54:18 +0200

    now is good to run and play!

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASS"),
-        "HOST": "db",  # set in docker-compose.yml
+        "HOST": "postgres-db",  # set in docker-compose.yml
         "PORT": 5432,  # default postgres port
     }
 }
