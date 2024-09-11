fa516f74501efb496e1aa67862e687d719f8c71c
SirAllap <serallapdivad@gmail.com>
2024-09-11 18:17:23 +0200

    added new route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,60 @@
 /* eslint-disable no-unused-vars */
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
-import Expenses from './pages/Expenses'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Expenses from "./pages/Expenses";
+import Pokemon from "./pages/Pokemon";
 
 const Logout = () => {
-	localStorage.removeItem('access')
-	localStorage.removeItem('refresh')
-	return <Navigate to='/login' />
-}
+  localStorage.removeItem("access");
+  localStorage.removeItem("refresh");
+  return <Navigate to="/login" />;
+};
 
 const RegisterAndLogout = () => {
-	localStorage.removeItem('access')
-	localStorage.removeItem('refresh')
-	return <Register />
-}
+  localStorage.removeItem("access");
+  localStorage.removeItem("refresh");
+  return <Register />;
+};
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<ProtectedRoute>
-							<Home />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path='/expenses'
-					element={
-						<ProtectedRoute>
-							<Expenses />
-						</ProtectedRoute>
-					}
-				/>
-				<Route path='/login' element={<Login />} />
-				<Route path='/logout' element={<Logout />} />
-				<Route path='/register' element={<RegisterAndLogout />} />
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	)
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/expenses"
+          element={
+            <ProtectedRoute>
+              <Expenses />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pokemon"
+          element={
+            <ProtectedRoute>
+              <Pokemon />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
