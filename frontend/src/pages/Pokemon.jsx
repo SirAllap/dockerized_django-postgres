c14d8d71d50d17bfc11186c4002aa73a1bbb39b2
David PR <serallapdivad@gmail.com>
2024-09-14 19:37:26 +0200

    added expenses/home button

--- frontend/src/pages/Pokemon.jsx
+++ frontend/src/pages/Pokemon.jsx
@@ -1,36 +1,59 @@
-import { useEffect, useState } from "react";
-import { useGetAllPokemonsQuery } from "../services/pokemon";
-import PokemonDetails from "../components/PokemonDetails";
+import { useEffect, useState } from "react"
+import { useGetAllPokemonsQuery } from "../services/pokemon"
+import PokemonDetails from "../components/PokemonDetails"
+import { ToggleScreenMode } from "../components/ToggleScreenMode"
+import { LogoutButton } from "../components/LogoutButton"
+import { useNavigate } from "react-router-dom"
 
 const Pokemon = () => {
-  const [selectedPokemons, setSelectedPokemons] = useState([]);
-  const [loadNumber, setLoadNumber] = useState(20);
+  const navigate = useNavigate()
+  const [selectedPokemons, setSelectedPokemons] = useState([])
+  const [loadNumber, setLoadNumber] = useState(20)
   const {
     data: getAllPokemons,
     isLoading: loadingAllPokemons,
     isError: errorAllPokemons,
-  } = useGetAllPokemonsQuery({ limit: loadNumber });
+  } = useGetAllPokemonsQuery({ limit: loadNumber })
 
   // This effect triggers once all Pokemons are loaded
   useEffect(() => {
     if (getAllPokemons && getAllPokemons.results.length > 0) {
       const pokemonNames = getAllPokemons.results.map(
         (pokemon) => pokemon.name,
-      );
-      setSelectedPokemons(pokemonNames); // Store the list of names
+      )
+      setSelectedPokemons(pokemonNames) // Store the list of names
     }
-  }, [getAllPokemons]);
+  }, [getAllPokemons])
 
-  if (loadingAllPokemons) return <p>Loading...</p>;
-  if (errorAllPokemons) return <p>Error loading pokemons...</p>;
+  if (loadingAllPokemons) return <p>Loading...</p>
+  if (errorAllPokemons) return <p>Error loading pokemons...</p>
 
   return (
-    <div className="w-screen min-h-screen">
+    <div className="max-w-screen-xl mx-auto py-5 px-5">
+      <section className="max-w-screen-xl flex flex-row justify-end gap-5">
+        <button
+          className="border border-yellow-300 bg-yellow-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-yellow-500 transition-all duration-300"
+          onClick={() => navigate("/expenses")}
+        >
+          Add expenses
+        </button>
+        <button
+          className="border border-yellow-300 bg-yellow-500 py-2 px-5 text-white rounded-md hover:bg-transparent hover:text-yellow-500 transition-all duration-300"
+          onClick={() => navigate("/")}
+        >
+          Add notes
+        </button>
+        <ToggleScreenMode />
+        <LogoutButton />
+      </section>
       <div className="max-w-2xl max-h-[95dvh] py-10 mx-auto text-center overflow-y-auto">
         <h1 className="mb-5">Pokemon Details</h1>
         <section className="flex flex-wrap justify-center items-center gap-4">
-          {selectedPokemons.map((pokemonName) => (
-            <PokemonDetails key={pokemonName} name={pokemonName} />
+          {selectedPokemons.map((pokemonName, index) => (
+            <div key={pokemonName}>
+              <a href={getAllPokemons.results[index].url}>JSON</a>
+              <PokemonDetails key={pokemonName} name={pokemonName} />
+            </div>
           ))}
         </section>
         <button
@@ -41,7 +64,7 @@ const Pokemon = () => {
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Pokemon;
+export default Pokemon
