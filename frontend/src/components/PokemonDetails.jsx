4801c0f39ef05037bdc642a396419f60b5aa0dc6
David PR <serallapdivad@gmail.com>
2024-09-14 19:36:50 +0200

    small changes on style

--- frontend/src/components/PokemonDetails.jsx
+++ frontend/src/components/PokemonDetails.jsx
@@ -1,18 +1,20 @@
-import { useGetPokemonByNameQuery } from "../services/pokemon";
+import { useGetPokemonByNameQuery } from "../services/pokemon"
 
 const PokemonDetails = ({ name }) => {
-  const { data: pokemon, isLoading, isError } = useGetPokemonByNameQuery(name);
+  const { data: pokemon, isLoading, isError } = useGetPokemonByNameQuery(name)
 
-  if (isLoading) return <p>Loading {name}...</p>;
-  if (isError) return <p>Error loading {name}</p>;
+  if (isLoading) return <p>Loading {name}...</p>
+  if (isError) return <p>Error loading {name}</p>
+  if (!pokemon) return <p>No data</p>
+  console.log(pokemon)
 
   return (
-    <div className="border-2 border-dotted border-indigo-400 p-5 w-fit text-center">
-      <h2>{pokemon.name}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    <div className="group border-2 border-dotted border-purple-400 rounded-md p-5 w-fit text-center hover:bg-purple-400/20 hover:scale-[1.02] transition-all duration-300">
+      <h2 className="font-bold capitalize">{pokemon.name}</h2>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} className="group-hover:scale-110 transition-all duration-300" />
       {/* Display other details as needed */}
     </div>
-  );
-};
+  )
+}
 
-export default PokemonDetails;
+export default PokemonDetails
